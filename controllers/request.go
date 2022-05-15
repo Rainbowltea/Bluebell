4c46bc1b149ae19ed7c715b60aaedc55e1a08b67
XiangXuhuai <1506347180@qq.com>
2022-05-15 21:48:34 +0800

    优化JWT

---
+++ controllers/request.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+var ErrorUserNotLogin = errors.New("用户未登录")
+
+const CtxUserIDKey = "userID"
+
+// Go中不支持包的互相调用，此处调用middlerwares中的变量，而middlerwares中又调用controlllers中的response
+func getCurrentUser(c *gin.Context) (userId int64, err error) {
+	uid, ok := c.Get(CtxUserIDKey)
+	if !ok {
+		err = ErrorUserNotLogin
+	}
+	//考虑转化失败的情况
+	userId, ok = uid.(int64)
+	if !ok {
+		err = ErrorUserNotLogin
+		return
+	}
+	return
+}
--- middlerwares/auth.go
+++ middlerwares/auth.go
@@ -1,8 +1,9 @@
 package middlerwares
 
 import (
+	"bluebell/controllers"
 	"bluebell/pkg/jwt"
-	"net/http"
+	_ "net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -16,35 +17,26 @@ func JWTAuthMiddleware() func(c *gin.Context) {
 		// 这里的具体实现方式要依据你的实际业务情况决定
 		authHeader := c.Request.Header.Get("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2003,
-				"msg":  "请求头中auth为空",
-			})
+			controllers.ResponseError(c, controllers.CodeNeedLogin)
 			c.Abort()
 			return
 		}
 		// 按空格分割
 		parts := strings.SplitN(authHeader, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2004,
-				"msg":  "请求头中auth格式有误",
-			})
+			controllers.ResponseError(c, controllers.CodeInvalidToken)
 			c.Abort()
 			return
 		}
 		// parts[1]是获取到的tokenString，我们使用之前定义好的解析JWT的函数来解析它
 		mc, err := jwt.ParseToken(parts[1])
 		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2005,
-				"msg":  "无效的Token",
-			})
+			controllers.ResponseError(c, controllers.CodeInvalidToken)
 			c.Abort()
 			return
 		}
 		// 将当前请求的userID信息保存到请求的上下文c上
-		c.Set("userID", mc.UserID)
+		c.Set(controllers.CtxUserIDKey, mc.UserID)
 		c.Next() // 后续的处理函数可以用过c.Get("userID")来获取当前请求的用户信息
 	}
 }
